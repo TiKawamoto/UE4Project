c477c99c5f2adde2a2fb5a96dfcb7af2ece84624
Ti <tikawamoto@gmail.com>
2017-02-05 19:13:37 -0500

    First Commit

---
+++ Section_02/BullCowGame/FBullCowGame.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class FBullCowGame 
+{
+public:
+	int GetMaxTries() const;
+	int GetCurrentTry() const;
+	bool IsGameWon() const;
+	
+	void Reset(); // TODO make a more rich return value
+	bool CheckGuessValidity(std::string); //TODO Make more rich return value
+
+
+private:
+	int MyCurrentTry = 1;
+	int MyMaxTries = 5;
+
+};
---
+++ Section_02/BullCowGame/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+void PrintIntro();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; // instantiate a new game
+
+// entry point for application
+int main() 
+{
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
+	
+	return 0; // exit the application
+}
+
+
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORD_LENGTH = 9;
+
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram I'm thinking of? \n";
+	std::cout << std::endl;
+	return;
+}
+
+// main gameplay
+void PlayGame()
+{
+	int MaxTries = BCGame.GetMaxTries();
+
+	for (int count = 0; count <= MaxTries; count++)
+	{
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+// get a guess from the player
+std::string GetGuess() 
+{
+	int CurrentTry = BCGame.GetCurrentTry();
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
+
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again (y/n)? ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+
+	std::cout << std::endl;
+
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
